d57d467786f6e66b3031ca9a21b0336047131180
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-07-20 09:54:55 -0600

    Update example.php

--- example.php
+++ example.php
@@ -3,10 +3,11 @@
 require 'vendor/autoload.php';
 
 // namespace config
-use \Pardot\API as API;
+use Pardot\Core\API as PardotAPI;
+use Pardot\Core\API as PardotConfig;
 
 //See Config source for more details
-$pardot_config = new \Pardot\Config(
+$pardot_config = new PardotConfig(
     array(
     'email' => "<YOUR EMAIL>",
     'password' => "<YOUR PASSWORD>",
@@ -16,23 +17,23 @@ $pardot_config = new \Pardot\Config(
 
 // read email by id
 echo 'Get email by ID:' . "\n";
-$email = API::Instance($pardot_config)->doOperationById(API::OBJ_EMAIL, API::OPR_READ, 447822116);
+$email = PardotAPI::Instance($pardot_config)->doOperationById(PardotAPI::OBJ_EMAIL, PardotAPI::OPR_READ, 447822116);
 var_dump($email['response']->email);
 
 // read prospect by email
 echo 'Get prospect by email:' . "\n";
-$prospect = API::Instance($pardot_config)->doOperationByField(
-    API::OBJ_PROSPECT,
-    API::OPR_READ,
+$prospect = PardotAPI::Instance($pardot_config)->doOperationByField(
+    PardotAPI::OBJ_PROSPECT,
+    PardotAPI::OPR_READ,
     'email',
     'email@example.com'
 );
 var_dump($prospect);
 
 // read all prospects updated in the last 15 minutes
-$prospects = API::Instance($pardot_config)->queryObject(API::OBJ_PROSPECT, array('updated_after' => '15 minutes ago'));
+$prospects = PardotAPI::Instance($pardot_config)->queryObject(PardotAPI::OBJ_PROSPECT, array('updated_after' => '15 minutes ago'));
 var_dump($prospects);
 
 //read all forms created after 10 days
-$forms = API::Instance($pardot_config)->queryObject(API::OBJ_FORM, array('created_after' => '10 days ago'));
+$forms = PardotAPI::Instance($pardot_config)->queryObject(PardotAPI::OBJ_FORM, array('created_after' => '10 days ago'));
 var_dump($forms);
